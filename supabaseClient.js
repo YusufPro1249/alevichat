bc778726522df67b4b1e9e1968c264529825e9cb
YusufPro1249 <140395337+YusufPro1249@users.noreply.github.com>
2026-05-01 14:56:58 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "BURAYA_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "BURAYA_ANON_KEY";
+const SUPABASE_URL = "sb_publishable_Sro0yq_dKmWTIxAnqULt5A_xbXun7O3";
+const SUPABASE_ANON_KEY = "sb_secret_fordfy0xiwmeHHtfHWhthA_6HTvVpck";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
