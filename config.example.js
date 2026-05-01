5087f9faee8fe1da9f7af16a75caaa25d3bd3b01
YusufPro1249 <140395337+YusufPro1249@users.noreply.github.com>
2026-05-01 21:39:34 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 window.__ALEVICHAT_CONFIG__ = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://pxfryssfvwdzgbkgrcud.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZnJ5c3Nmdndkemdia2dyY3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MjIwMzksImV4cCI6MjA5MzE5ODAzOX0.S__p_B4G63ifd64FsHOR9MlRpulSRLMbSvYdbnNhEUQ",
 };
